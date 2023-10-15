a7f68c96820481b48352f3761cf83a0985b8a5cb
Calebe Machado <calebe.machado@ifood.com.br>
2023-10-15 17:25:03 -0300

    Add super digit solution

---
+++ HackerRank/1 Week Preparation Kit/Day 4/Java/SuperDigit.java
@@ -0,0 +1,28 @@
+class Result {
+
+    /*
+     * Complete the 'superDigit' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING n
+     *  2. INTEGER k
+     */
+
+    public static int superDigit(String n, int k) {
+        boolean allDigitsSame = true;
+        for(int i = n.length() - 1; i > 1; i--) {
+            if(n.charAt(0) != n.charAt(i)) {
+              allDigitsSame = false;
+              break;
+            }
+        }
+        
+        char nine = '9';
+        if(allDigitsSame && n.charAt(0) == nine) return 9;
+        
+        Integer superD = isSuperDigit(n) * k;
+        
+        return isSuperDigit(superD.toString());    
+    }
+}
